dae9ae3bb75ad6698233fac93ce6eef53fbb546f
BrunoZunino <bruno.zunino@anima.edu.uy>
2019-05-24 14:33:17 -0300

    subo app pero con error en los estilos

---
+++ my-app/src/components/header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './header.css';
+
+function Header() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p styles={a}>
+          Bienvenidos a PedidosYa
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
