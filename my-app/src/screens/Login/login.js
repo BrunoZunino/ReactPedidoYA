6c2bc644be53e5a7fd6f2d63e0c9cac8f3ff7fb2
BrunoZunino <bruno.zunino@anima.edu.uy>
2019-06-03 07:50:50 -0300

    Subo diseño de login bastante avanzado, falta la imagen de el logo y vincular los botones

--- my-app/src/screens/Login/login.js
+++ my-app/src/screens/Login/login.js
@@ -2,22 +2,39 @@ import React from 'react';
 import styles from './login.css';
 
 class Login extends React.Component {
-  Constructor(props){
+  constructor(props){
+    super(props);
+
+    this.state={
+      email: "",
+      pass: "",
+    };
+  }
+
+  componentDidMount() {
+    console.log('I was triggered during componentDidMount');
   }
 
   render(){
+    
+    console.log('email: ', this.state.email);
+    console.log('pass: ', this.state.pass);
     return (
       <div class="main">
         <div class="container">
           <div class="login">
               <form>
-                <div class="form-group">
-                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+                <div class="form-group emailinput">
+                  <input  value={this.state.email} onChange={(event) => {this.setState({email: event.target.value});}} type="email" class="form-control emailInput" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email"/>
                 </div>
-                  <div class="form-group">
-                  <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+                  <div class="form-group passInput">
+                  <input value={this.state.pass} onChange={(event) => {this.setState({pass: event.target.value});}} type="password" class="form-control passInput" id="exampleInputPassword1" placeholder="Password"/>
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-primary loginButton ">Log in</button>
+                <div class="plusButtons">
+                  <button type="submit" class="btn btn-primary facebookButton ">Facebook</button>
+                  <button type="submit" class="btn btn-primary gmailButton ">Gmail</button>
+                </div>
             </form>
           </div>
         </div>
